5040734969dc9547c707cbf4aac4c7dbcf812456
Tolly <34184902+TollyH@users.noreply.github.com>
2022-12-21 02:29:36 +0000

    Create disassembler

---
+++ Disassembler.cs
@@ -0,0 +1,138 @@
+﻿using System.Buffers.Binary;
+
+namespace AssEmbly
+{
+    public static class Disassembler
+    {
+        /// <summary>
+        /// Disassemble a program to AssEmbly code from it's assembled bytecode.
+        /// </summary>
+        public static string DisassembleProgram(byte[] program)
+        {
+            int offset = 0;
+            List<string> result = new();
+            Dictionary<int, int> offsetToLine = new();
+            List<(ulong, int)> references = new();
+            while (offset < program.Length)
+            {
+                offsetToLine[offset] = result.Count;
+                (string line, int additionalOffset, ulong? referencedAddress) = DisassembleInstruction(program[offset..]);
+                offset += additionalOffset;
+                if (referencedAddress is not null)
+                {
+                    references.Add((referencedAddress.Value, result.Count));
+                }
+                result.Add(line);
+            }
+            references.Sort((a, b) => a.Item1.CompareTo(b.Item1));
+            references = references.DistinctBy(a => a.Item1).ToList();
+            int inserted = 0;
+            foreach ((ulong address, int sourceLineIndex) in references)
+            {
+                if (offsetToLine.TryGetValue((int)address, out int destLineIndex))
+                {
+                    result.Insert(destLineIndex + inserted, $":ADDR_{address:X}");
+                    inserted++;
+                }
+                else
+                {
+                    result = result.Select(s => s.Replace($":ADDR_{address:X}", ":INVALID-LABEL")).ToList();
+                }
+            }
+            return string.Join("\n", result);
+        }
+
+        /// <summary>
+        /// Disassemble a single line of AssEmbly code from it's assembled bytecode.
+        /// </summary>
+        /// <param name="instruction">The instruction to disassemble. More bytes than needed may be given.</param>
+        /// <returns>(Disassembled line, Number of bytes instruction was, Referenced address [if present])</returns>
+        public static (string, int, ulong?) DisassembleInstruction(byte[] instruction)
+        {
+            bool fallbackToDat = false;
+
+            byte opcode = instruction[0];
+            if (opcode == 0)
+            {
+                string padString = "HLT";
+                int byteCount = 1;
+                if (instruction.Length > 1 && instruction[1] == 0)
+                {
+                    for (int i = 1; i < instruction.Length && instruction[i] == 0; i++)
+                    {
+                        byteCount++;
+                    }
+                    padString += $"\nPAD {byteCount - 1}";
+                }
+                return (padString, byteCount, null);
+            }
+            IEnumerable<KeyValuePair<(string, Data.OperandType[]), byte>> matching = Data.Mnemonics.Where(x => x.Value == opcode);
+            if (!matching.Any())
+            {
+                fallbackToDat = true;
+            }
+            else
+            {
+                (string mnemonic, Data.OperandType[] operandTypes) = matching.First().Key;
+                List<string> operandStrings = new();
+                int totalBytes = 1;
+                ulong? referencedAddress = null;
+                foreach (Data.OperandType type in operandTypes)
+                {
+                    if (totalBytes >= instruction.Length)
+                    {
+                        fallbackToDat = true;
+                        break;
+                    }
+                    switch (type)
+                    {
+                        case Data.OperandType.Register:
+                            if (Enum.IsDefined((Data.Register)instruction[totalBytes]))
+                            {
+                                operandStrings.Add(((Data.Register)instruction[totalBytes]).ToString());
+                                totalBytes++;
+                            }
+                            else
+                            {
+                                fallbackToDat = true;
+                            }
+                            break;
+                        case Data.OperandType.Literal:
+                            operandStrings.Add(BinaryPrimitives.ReadUInt64LittleEndian(instruction.AsSpan()[totalBytes..(totalBytes + 8)]).ToString());
+                            totalBytes += 8;
+                            break;
+                        case Data.OperandType.Address:
+                            referencedAddress = BinaryPrimitives.ReadUInt64LittleEndian(instruction.AsSpan()[totalBytes..(totalBytes + 8)]);
+                            operandStrings.Add($":ADDR_{referencedAddress:X}");
+                            totalBytes += 8;
+                            break;
+                        case Data.OperandType.Pointer:
+                            if (Enum.IsDefined((Data.Register)instruction[totalBytes]))
+                            {
+                                operandStrings.Add("*" + ((Data.Register)instruction[totalBytes]).ToString());
+                                totalBytes++;
+                            }
+                            else
+                            {
+                                fallbackToDat = true;
+                            }
+                            break;
+                        default:
+                            fallbackToDat = true;
+                            break;
+                    }
+                    if (fallbackToDat)
+                    {
+                        break;
+                    }
+                }
+                if (!fallbackToDat)
+                {
+                    return ($"{mnemonic} {string.Join(", ", operandStrings)}", totalBytes, referencedAddress);
+                }
+            }
+
+            return ($"DAT {instruction[0]}", 1, null);
+        }
+    }
+}
