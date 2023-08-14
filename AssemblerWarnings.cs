86bcc19c0e918853690d7c8448ec095bae254484
TollyH <ptolemyhill@sky.com>
2023-08-14 14:29:57 +0100

    Add outlines for final analyzers

--- AssemblerWarnings.cs
+++ AssemblerWarnings.cs
@@ -31,17 +31,32 @@
     public partial class AssemblerWarnings
     {
 
-        private static readonly Dictionary<int, WarningAnalyzer> nonFatalErrorAnalyzers = new()
+        private static readonly Dictionary<int, WarningAnalyzer> nonFatalErrorRollingAnalyzers = new()
         {
             // TODO: Implement
         };
 
-        private static readonly Dictionary<int, WarningAnalyzer> warningAnalyzers = new()
+        private static readonly Dictionary<int, WarningAnalyzer> warningRollingAnalyzers = new()
         {
             // TODO: Implement
         };
 
-        private static readonly Dictionary<int, WarningAnalyzer> suggestionAnalyzers = new()
+        private static readonly Dictionary<int, WarningAnalyzer> suggestionRollingAnalyzers = new()
+        {
+            // TODO: Implement
+        };
+
+        private static readonly Dictionary<int, WarningAnalyzer> nonFatalErrorFinalAnalyzers = new()
+        {
+            // TODO: Implement
+        };
+
+        private static readonly Dictionary<int, WarningAnalyzer> warningFinalAnalyzers = new()
+        {
+            // TODO: Implement
+        };
+
+        private static readonly Dictionary<int, WarningAnalyzer> suggestionFinalAnalyzers = new()
         {
             // TODO: Implement
         };
@@ -67,6 +82,16 @@
         /// <param name="newBytes">The bytes of the next instruction to check for warnings.</param>
         /// <returns>An array of any warnings caused by the new instruction.</returns>
         public Warning[] NextInstruction(byte[] newBytes)
+        {
+
+        }
+
+        /// <summary>
+        /// Call this after all program instructions have been given to <see cref="NextInstruction"/>
+        /// to run analyzers that need the entire program to work.
+        /// </summary>
+        /// <returns>An array of any warnings caused by final analysis.</returns>
+        public Warning[] Finalize()
         {
 
         }
