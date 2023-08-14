7782f6ce1a10f5fd97d40a03d82bc153b0541ee0
TollyH <ptolemyhill@sky.com>
2023-08-14 14:05:18 +0100

    Warning messages and class outline

---
+++ AssemblerWarnings.Messages.cs
@@ -0,0 +1,56 @@
+﻿namespace AssEmbly
+{
+    public partial class AssemblerWarnings
+    {
+        public static readonly Dictionary<int, string> NonFatalErrorMessages = new()
+        {
+            { 0001, "Instruction writes to the rpo register." },
+            { 0002, "Division by constant 0." },
+        };
+
+        public static readonly Dictionary<int, string> WarningMessages = new()
+        {
+            { 0001, "Data insertion is not directly preceded by an unconditional jump, return, or halt instruction." },
+            { 0002, "Jump/Call target label points to data, not executable code." },
+            { 0003, "Jump/Call target label points to end of file, not executable code." },
+            { 0004, "Instruction writes to a label pointing to executable code." },
+            { 0005, "Instruction reads from a label pointing to executable code in a context that likely expects data." },
+            { 0006, "Labelled string insertion is not immediately followed by a 0 (null) byte." },
+            { 0007, "Numeric literal is too large for the given move instruction. Upper bits will be truncated at runtime." },
+            { 0008, "Unreachable code detected." },
+            { 0009, "Program runs to end of file without being terminated by an unconditional jump, return, or halt instruction." },
+            { 0010, "File import is not directly preceded by an unconditional jump, return, or halt instruction." },
+            { 0011, "Instruction writes to the rsf register." },
+            { 0012, "Instruction writes to the rsb register." },
+            { 0013, "Jump/Call target label points to itself, resulting in an unbreakable infinite loop." },
+        };
+
+        public static readonly Dictionary<int, string> SuggestionMessages = new()
+        {
+            { 0001, "Avoid use of NOP instruction." },
+            { 0002, "Use the `PAD` directive instead of chaining `DAT 0` directives." },
+            { 0003, "Put IMP directives at the end of the file, unless the position of the directive is important given the file's contents." },
+            { 0004, "Put data at the end of the file, unless the position of the data is important." },
+            { 0005, "Use `TST {1}, {1}` instead of `CMP {1}, 0`, as it results in less bytes." },
+            { 0006, "Use `TST {2}, {2}` instead of `CMP 0, {2}`, as it results in less bytes." },
+            { 0007, "Use `XOR {1}, {1}` instead of `{0} {1}, 0`, as it results in less bytes." },
+            { 0008, "Use `INC {1}` instead of `ADD {1}, 1`, as it results in less bytes." },
+            { 0009, "Use `DEC {1}` instead of `SUB {1}, 1`, as it results in less bytes." },
+            { 0010, "Put CMP and TST instructions directly before their respective conditional jumps for clarity." },
+            { 0011, "Operation has no effect." },
+            { 0012, "Shift operation shifts by 64 bits or more, which will always result in 0. Use `XOR {1}, {1}` instead." },
+            { 0013, "Remove leading 0 digits from denary number." },
+        };
+
+        public static Dictionary<int, string> GetMessagesForSeverity(WarningSeverity severity)
+        {
+            return severity switch
+            {
+                WarningSeverity.NonFatalError => NonFatalErrorMessages,
+                WarningSeverity.Warning => WarningMessages,
+                WarningSeverity.Suggestion => SuggestionMessages,
+                _ => throw new ArgumentException("Given severity is not valid.")
+            };
+        }
+    }
+}
---
+++ AssemblerWarnings.cs
@@ -0,0 +1,74 @@
+﻿namespace AssEmbly
+{
+    public enum WarningSeverity
+    {
+        NonFatalError,
+        Warning,
+        Suggestion
+    }
+
+    [Serializable]
+    public readonly struct Warning
+    {
+        public readonly WarningSeverity Severity;
+        public readonly int Code;
+        public readonly int Line;
+        public readonly int Column;
+
+        public string Message => AssemblerWarnings.GetMessagesForSeverity(Severity)[Code];
+
+        public Warning(WarningSeverity severity, int code, int line, int column)
+        {
+            Severity = severity;
+            Code = code;
+            Line = line;
+            Column = column;
+        }
+    }
+
+    public delegate bool WarningAnalyzer(byte[] newBytes);
+
+    public partial class AssemblerWarnings
+    {
+
+        private static readonly Dictionary<int, WarningAnalyzer> nonFatalErrorAnalyzers = new()
+        {
+            // TODO: Implement
+        };
+
+        private static readonly Dictionary<int, WarningAnalyzer> warningAnalyzers = new()
+        {
+            // TODO: Implement
+        };
+
+        private static readonly Dictionary<int, WarningAnalyzer> suggestionAnalyzers = new()
+        {
+            // TODO: Implement
+        };
+
+        public HashSet<bool> EnabledNonFatalErrors = new()
+        {
+            // TODO: All by default
+        };
+
+        public HashSet<bool> EnabledWarnings = new()
+        {
+            // TODO: All by default
+        };
+
+        public HashSet<bool> EnabledSuggestions = new()
+        {
+            // TODO: All by default
+        };
+
+        /// <summary>
+        /// Update the state of the class instance with the next instruction in the program being analyzed.
+        /// </summary>
+        /// <param name="newBytes">The bytes of the next instruction to check for warnings.</param>
+        /// <returns>An array of any warnings caused by the new instruction.</returns>
+        public Warning[] NextInstruction(byte[] newBytes)
+        {
+
+        }
+    }
+}
