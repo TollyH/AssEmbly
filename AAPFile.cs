316b80ce8786f7ad5e4b1006f4ef14665332877d
TollyH <ptolemyhill@sky.com>
2023-08-23 18:47:50 +0100

    Initial AAPFile class structure

---
+++ AAPFile.cs
@@ -0,0 +1,31 @@
+﻿namespace AssEmbly
+{
+    [Flags]
+    public enum AAPFeatures
+    {
+        V1CallStack = 0b1,
+        ExtensionSigned = 0b10,
+        ExtensionFloat = 0b100,
+    }
+
+    public class AAPFile
+    {
+        // "AssEmbly"
+        public static readonly byte[] MagicBytes = { 65, 115, 115, 69, 109, 98, 108, 121 };
+
+        // Header
+        public Version LanguageVersion { get; set; }
+        public AAPFeatures Features { get; set; }
+        public ulong EntryPoint { get; set; }
+
+        public byte[] Program { get; set; }
+
+        public AAPFile(Version languageVersion, AAPFeatures features, ulong entryPoint, byte[] program)
+        {
+            LanguageVersion = languageVersion;
+            Features = features;
+            EntryPoint = entryPoint;
+            Program = program;
+        }
+    }
+}
