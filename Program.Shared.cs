8fc9262769b2b9078c60bbea6efa369a01757057
TollyH <ptolemyhill@sky.com>
2023-09-08 14:18:54 +0100

    Show floating point value of registers in debugger

--- Program.Shared.cs
+++ Program.Shared.cs
@@ -193,6 +193,12 @@
                 Console.Write($"    {Enum.GetName((Register)register)}: {value}");
                 if (value != 0)
                 {
+                    double floatingValue = BitConverter.UInt64BitsToDouble(value);
+                    // Don't print extreme values as floating point
+                    if (floatingValue is >= 0.0000000000000001 and <= ulong.MaxValue)
+                    {
+                        Console.Write($" ({floatingValue:0.0###############})");
+                    }
                     if (value >= 10)
                     {
                         Console.Write($" (0x{value:X})");
