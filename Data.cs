67aeb90d9feb5d11fac31ce577fd958bff357550
TollyH <ptolemyhill@sky.com>
2024-02-10 01:28:46 +0000

    Move MnemonicComparer into Data.cs

--- Data.cs
+++ Data.cs
@@ -639,4 +639,17 @@
             { ("HEAP_FRE", new OperandType[1] { OperandType.Register }), new Opcode(0x05, 0x20) },
         };
     }
+
+    public class MnemonicComparer : EqualityComparer<(string Mnemonic, OperandType[] OperandTypes)>
+    {
+        public override bool Equals((string Mnemonic, OperandType[] OperandTypes) first, (string Mnemonic, OperandType[] OperandTypes) second)
+        {
+            return first.Mnemonic == second.Mnemonic && first.OperandTypes.SequenceEqual(second.OperandTypes);
+        }
+
+        public override int GetHashCode((string Mnemonic, OperandType[] OperandTypes) obj)
+        {
+            return obj.Mnemonic.GetHashCode();
+        }
+    }
 }
