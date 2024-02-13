4a90efdd1314b777761d3431ac67d59e531a5628
TollyH <ptolemyhill@sky.com>
2024-02-13 01:38:34 +0000

    Create build script

--- build-docs.ps1
+++ build-docs.ps1
@@ -1,10 +1,14 @@
 # ReferenceManual.md is the source of the documentation,
 # edits to the other files (txt, html, docx, pdf) will be overwritten.
 # Requires pandoc to be installed and on PATH
+Write-Output "    Building .txt..."
 pandoc -t plain -s .\Documentation\ReferenceManual\ReferenceManual.md -o .\Documentation\ReferenceManual\ReferenceManual.txt
+Write-Output "    Building .html..."
 pandoc -s .\Documentation\ReferenceManual\ReferenceManual.md --highlight-style breezedark  -o .\Documentation\ReferenceManual\ReferenceManual.html --metadata pagetitle="AssEmbly Reference Manual"
+Write-Output "    Building .docx..."
 pandoc -s .\Documentation\ReferenceManual\ReferenceManual.md --highlight-style breezedark -o .\Documentation\ReferenceManual\ReferenceManual.docx
 
+Write-Output "    Building .pdf..."
 $word_app = New-Object -ComObject Word.Application
 $document = $word_app.Documents.Open([IO.Path]::GetFullPath(".\Documentation\ReferenceManual\ReferenceManual.docx"))
 $pdf_filename = [IO.Path]::GetFullPath(".\Documentation\ReferenceManual\ReferenceManual.pdf")
---
+++ build.ps1
@@ -0,0 +1,31 @@
+$publishFolder = "Publish"
+
+Write-Output "`nDeleting existing Publish folder..."
+if (Test-Path $publishFolder) {
+    Remove-Item $publishFolder -Recurse -Force
+}
+
+$publishProfiles = Get-ChildItem "Properties/PublishProfiles" -Filter *.pubxml
+foreach ($file in $publishProfiles) {
+    $profileName = [System.IO.Path]::GetFileNameWithoutExtension($file.Name)
+    Write-Output "`nStarting build for $profileName..."
+    dotnet publish AssEmbly.csproj -p:PublishProfile="Properties/PublishProfiles/$profileName.pubxml" -p:TreatWarningsAsErrors=true -warnaserror
+}
+
+Write-Output ""
+$subFolders = Get-ChildItem $publishFolder -Directory
+foreach ($folder in $subFolders) {
+    $zipName = "AssEmbly-" + $folder.Name + ".zip"
+    Write-Output "Compressing into $zipName..."
+    $zipPath = Join-Path $publishFolder $zipName
+    Get-ChildItem -Path $folder -Exclude "*.pdb" |
+        Compress-Archive -DestinationPath $zipPath -CompressionLevel Optimal
+}
+
+Write-Output "`nBuilding documentation..."
+.\build-docs.ps1
+$docsZipPath = Join-Path $publishFolder "Documentation.zip"
+Write-Output "`nCompressing documentation..."
+Compress-Archive -Path "Documentation\ReferenceManual\ReferenceManual.*" -DestinationPath $docsZipPath -CompressionLevel Optimal
+
+Write-Output "Done."
